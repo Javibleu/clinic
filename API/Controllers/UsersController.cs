968eadb034ef92e9a21bde648d05d5691c7b07e8
Javi <javibleu@hotmail.com>
2021-01-13 10:54:04 -0500

    First Push

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext context;            //injected context field
+
+        public UsersController(DataContext context)     //constructor
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await context.Users.ToListAsync();
+        }
+
+
+        //  when hits api/users/3
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await context.Users.FindAsync(id);
+        }
+
+
+    }
+}
